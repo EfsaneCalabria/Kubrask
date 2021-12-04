5be0a26a0332c7bc0308de608030eac93afd990b
zz.calabria <90386694+EfsaneCalabria@users.noreply.github.com>
2021-12-04 19:51:01 +0300

    Update reboot.js

--- komutlar/reboot.js
+++ komutlar/reboot.js
@@ -3,9 +3,9 @@ const bot = new Discord.Client();
 const ayarlar = require("../ayarlar.json") // eğer ayarlar.json değilse değiştirin burayı
 
 module.exports.run = async (bot, message, args) => {
-    if(message.author.id !== "852641826921250856") return message.channel.send("bunu sadece sahibim kullanabilir")
+    if(message.author.id !== "id") return message.channel.send("bunu sadece sahibim kullanabilir")
     
-    message.channel.send(`Calabria?`).then(msg => {
+    message.channel.send(`zort?`).then(msg => {
     console.log(`Konsol: Yeniden başlatılıyor...`);
     process.exit(0);
   })
